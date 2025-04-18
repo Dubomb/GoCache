e12ffb8d2aa2841c34b490b126987b90d589e730
Dubomb <dubomb1011@gmail.com>
2025-04-17 22:38:33 -0400

    Created struct for cache with set and get behavior

--- main.go
+++ main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
-func main()  {
+type GoCache struct {
+	cache map[string]string
+}
+
+func NewGoCache() *GoCache {
+	return &GoCache{
+		make(map[string]string),
+	}
+}
+
+func (g *GoCache) Set(key string, value string) {
+	g.cache[key] = value
+}
+
+func (g *GoCache) Get(key string) string {
+	return g.cache[key]
+}
+
+func main() {
 	fmt.Println("Hello world!")
+
+	g := NewGoCache()
+	g.Set("hello", "world")
+	fmt.Println(g)
+	fmt.Println(g.Get("hello"))
 }
