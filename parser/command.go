3937543f80820dd88ede6cf05a8f1ab6ff921140
Dubomb <dubomb1011@gmail.com>
2025-06-26 21:49:18 -0400

    Created data types for representing cache commands

---
+++ parser/command.go
@@ -0,0 +1,37 @@
+package parser
+
+import "strings"
+
+type CommandType string
+
+const (
+	SetCommand     CommandType = "SET"
+	GetCommand     CommandType = "GET"
+	DelCommand     CommandType = "DEL"
+	ExistsCommand  CommandType = "EXISTS"
+	UnknownCommand CommandType = "UNKNOWN"
+)
+
+type Command struct {
+	Type  CommandType
+	Key   string
+	Value string
+	Args  []string
+}
+
+func ToCommandType(s string) CommandType {
+	upper := CommandType(strings.ToUpper(s))
+
+	switch upper {
+	case SetCommand:
+		return SetCommand
+	case GetCommand:
+		return GetCommand
+	case DelCommand:
+		return DelCommand
+	case ExistsCommand:
+		return ExistsCommand
+	default:
+		return UnknownCommand
+	}
+}
